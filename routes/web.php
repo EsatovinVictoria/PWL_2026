8596864f84a80b5bf5d2bd21660861496b1bffaf
Esatovin Ebenhaezer Victoria <esatovinv@gmail.com>
2026-02-25 21:58:28 +0700

    Jobsheet2_Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,33 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return '244107020228<br>Esatovin Ebenhaezer Victoria';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . ', komentar ke-' . $commentId;
+});
+
+Route::get('/articles/{id}/', function ($id) {
+    return 'Halaman artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
 });
