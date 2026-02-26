216c23ee4d98b278b97f92fc67bdd9779586f32a
Esatovin Ebenhaezer Victoria <esatovinv@gmail.com>
2026-02-26 09:11:43 +0700

    Jobsheet2_Praktikum3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,4 +9,10 @@ class WelcomeController extends Controller
     public function hello() {
         print 'Hello World';
     }
+
+    public function greeting() {
+        return view('blog.hello')
+            ->with('name', 'Esatovin')
+            ->with('occupation', 'Web Developer');
+    }
 }
